0852a1bff5f344d12ff9f1ca7d1488146c287fa8
JHuaWei <2237268069@qq.com>
2023-05-14 14:40:31 +0800

    创建 欢迎 组件

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,11 +1,38 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, RouteRecordRaw, createWebHashHistory } from "vue-router";
 import { Foo } from "../views/Foo";
 import { Bar } from "../views/Bar";
 import { history } from "../shared/history";
+import { First } from "../components/welcome/first";
+import { Second } from "../components/welcome/second";
+import { Third } from "../components/welcome/third";
+import { Forth } from "../components/welcome/forth";
+import { Welcome } from "../views/Welcome";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   { path: "/", component: Foo },
   { path: "/about", component: Bar },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      {
+        path: "1",
+        component: First,
+      },
+      {
+        path: "2",
+        component: Second,
+      },
+      {
+        path: "3",
+        component: Third,
+      },
+      {
+        path: "4",
+        component: Forth,
+      },
+    ],
+  },
 ];
 
 export const router = createRouter({
